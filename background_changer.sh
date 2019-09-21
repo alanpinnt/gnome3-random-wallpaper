5366291093aadcf79a3e02fc554560bc3ea215cc
Alan Pinnt <alan.pinnt@gmail.com>
2019-09-21 15:26:00 -0500

    Script to run to change wallpaper

---
+++ background_changer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PID=$(pgrep --euid $EUID gnome-session)
+export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
+
+DIR="/files/to/images/to/randomly/go/through"
+PIC=$(ls $DIR/* | shuf -n1)
+gsettings set org.gnome.desktop.background picture-uri "file://$PIC"
+gsettings set org.gnome.desktop.screensaver picture-uri "file://$PIC"
